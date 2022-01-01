702c3294b05e0e98b0816247db23507f4d23e1de
Julian Eigmüller <j.eigmueller17@gmail.com>
2022-01-01 16:29:43 +0100

    Add option to change format to de or us (def: de)

--- shime.c
+++ shime.c
@@ -42,15 +42,20 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #define NANO_INTERVAL 25000000
 
-void finish(int sig);
+typedef struct {
+    char* fmt;
+    char* last_next;
+} DateTimeFormat;
 
-void get_last_next_hours_mins_or_seconds(int n, int max, int *last, int *next);
-void get_last_next_time(struct tm cur, struct tm *last, struct tm *next);
+const DateTimeFormat de = {
+    .fmt       = "%d.%m.%Y %H:%M:%S",
+    .last_next = "%d %m %Y %H %M %S",
+};
 
-int days_in_month(int mon, int year);
-
-const char* format           = "%d.%m.%Y %H:%M:%S";
-const char* format_last_next = "%d %m %Y %H %M %S";
+const DateTimeFormat us = {
+    .fmt       = "%m/%d/%Y %H:%M:%S",
+    .last_next = "%m %d %Y %H %M %S",
+};
 
 typedef struct {
     int y;
@@ -59,36 +64,166 @@ typedef struct {
     int width;
 } Dimensions;
 
+DateTimeFormat strtimeformat(char* str);
+
+void finish(int sig);
+
+void get_last_next_hours_mins_or_seconds(int n, int max, int *last, int *next);
+void get_last_next_time(struct tm cur, struct tm *last, struct tm *next);
+
+int days_in_month(int mon, int year);
+
+DateTimeFormat strtimeformat(char* str)
+{
+    if(strcmp(str, "de") == 0) {
+        return de;
+    } else if(strcmp(str, "us") == 0) {
+        return us;
+    } else {
+        fprintf(stderr, "Invalid format: '%s'\n", str);
+        exit(1);
+    }
+}
+
+// Cleanly exit ncurses
+void finish(int sig)
+{
+    endwin();
+    if (sig == SIGSEGV) {
+        fprintf(stderr, "Segfault\n");
+        exit(1);
+    }
+    exit(0);
+}
+
+void get_last_next_hours_mins_or_seconds(int n, int max, int *last, int *next)
+{
+    if (n + 1 >= max) {
+        *next = 0;
+    } else {
+        *next = n + 1;
+    }
+    if (n - 1 <= -1) {
+        *last = max - 1;
+    } else {
+        *last = n - 1;
+    }
+}
+
+// Calculate the last and the next time unit for every unit in cur
+void get_last_next_time(struct tm cur, struct tm *last, struct tm *next)
+{
+    int year = cur.tm_year + 1900;
+    int mon = cur.tm_mon + 1;
+
+    // Days
+    if (cur.tm_mday + 1 > days_in_month(mon, year)) {
+        next->tm_mday = 1;
+    } else {
+        next->tm_mday = cur.tm_mday + 1;
+    }
+    if (cur.tm_mday - 1 <= 0) {
+        if (mon - 1 <= 0) {
+            // Last day is in December of last year
+            last->tm_mday = days_in_month(12, year - 1);
+        } else {
+            last->tm_mday = days_in_month(mon - 1, year);
+        }
+    } else {
+        last->tm_mday = cur.tm_mday - 1;
+    }
+
+    // Months
+    if (mon + 1 == 13) {
+        next->tm_mon = 0;
+    } else {
+        next->tm_mon = cur.tm_mon + 1;
+    }
+    if (mon - 1 == 0) {
+        last->tm_mon = 11;
+    } else {
+        last->tm_mon = cur.tm_mon - 1;
+    }
+
+    // Years
+    next->tm_year = cur.tm_year + 1;
+    last->tm_year = cur.tm_year - 1;
+
+    get_last_next_hours_mins_or_seconds(cur.tm_hour, 24, &last->tm_hour,
+                                        &next->tm_hour); // Hours
+    get_last_next_hours_mins_or_seconds(cur.tm_min, 60, &last->tm_min,
+                                        &next->tm_min); // Minutes
+    get_last_next_hours_mins_or_seconds(cur.tm_sec, 60, &last->tm_sec,
+                                        &next->tm_sec); // Seconds
+}
+
+// Return the days a month has
+int days_in_month(int mon, int year)
+{
+    assert(mon >= 1 && mon <= 12);
+    switch (mon) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        return 30;
+    case 2:
+        // Leap year: https://en.wikipedia.org/wiki/Leap_year#Algorithm
+        if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))
+            return 29;
+        else
+            return 28;
+    default:
+        assert(false);
+        return 0;
+    }
+}
+
 int main(int argc, char **argv)
 {
+    DateTimeFormat format = de;
+
     // TODO: add other data/time formats
     // Argparse with getopt()
     int arg;
-    while ((arg = getopt(argc, argv, "hu")) != -1) {
+    while ((arg = getopt(argc, argv, "hf:")) != -1) {
         switch (arg) {
         case 'h':
             // Help page
             printf("shime Copyright (C) 2021 eyeofcthulhu\n\n"
                    "options:\n"
-                   "h: display help\n"
-                   "u: display usage\n\n"
+                   "-h: display help\n"
+                   "-f [de us]: change the time format\n"
+                   "-u: display usage\n\n"
                    "controls\n"
                    "vim keys - hjkl or arrow keys: move around\n"
                    "q or esc: exit\n");
             exit(0);
             break;
+        case 'f':
+            format = strtimeformat(optarg);
+            break;
+        case '?':
         default:
-        case 'u':
-            printf("Usage: %s [-hu]\n", argv[0]);
-            exit(0);
+            return 1;
             break;
         }
     }
 
     // Ncurses init logic
+
     // On interrupt (Ctrl+c) exit
     signal(SIGINT, finish);
-    // Signal(SIGSEGV, finish);
+    signal(SIGKILL, finish);
+    signal(SIGSEGV, finish);
 
     // Init
     initscr();
@@ -114,7 +249,7 @@ int main(int argc, char **argv)
     // Color support
     if (!has_colors()) {
         fprintf(stderr, "Your terminal does not support color\n");
-        finish(0);
+        finish(SIGKILL);
     }
 
     start_color();
@@ -193,7 +328,7 @@ int main(int argc, char **argv)
             attron(COLOR_PAIR(1));
             move(dimensions.y, dimensions.x);
 
-            strftime(buf, sizeof(buf), format, local_time);
+            strftime(buf, sizeof(buf), format.fmt, local_time);
 
             // Draw the date and time to the screen
             addstr(buf);
@@ -204,10 +339,10 @@ int main(int argc, char **argv)
 
             attron(COLOR_PAIR(2));
 
-            strftime(buf, sizeof(buf), format_last_next, &last);
+            strftime(buf, sizeof(buf), format.last_next, &last);
             mvaddstr(dimensions.y - 1, dimensions.x, buf);
 
-            strftime(buf, sizeof(buf), format_last_next, &next);
+            strftime(buf, sizeof(buf), format.last_next, &next);
             mvaddstr(dimensions.y + 1, dimensions.x, buf);
 
             refresh();
@@ -221,104 +356,4 @@ int main(int argc, char **argv)
     endwin();
 
     return 0;
-}
-
-// Cleanly exit ncurses
-void finish(int sig)
-{
-    endwin();
-    if (sig == SIGSEGV)
-        printf("Segfault\n");
-    exit(0);
-}
-
-void get_last_next_hours_mins_or_seconds(int n, int max, int *last, int *next)
-{
-    if (n + 1 >= max) {
-        *next = 0;
-    } else {
-        *next = n + 1;
-    }
-    if (n - 1 <= -1) {
-        *last = max - 1;
-    } else {
-        *last = n - 1;
-    }
-}
-
-// Calculate the last and the next time unit for every unit in cur
-void get_last_next_time(struct tm cur, struct tm *last, struct tm *next)
-{
-    int year = cur.tm_year + 1900;
-    int mon = cur.tm_mon + 1;
-
-    // Days
-    if (cur.tm_mday + 1 > days_in_month(mon, year)) {
-        next->tm_mday = 1;
-    } else {
-        next->tm_mday = cur.tm_mday + 1;
-    }
-    if (cur.tm_mday - 1 <= 0) {
-        if (mon - 1 <= 0) {
-            // Last day is in December of last year
-            last->tm_mday = days_in_month(12, year - 1);
-        } else {
-            last->tm_mday = days_in_month(mon - 1, year);
-        }
-    } else {
-        last->tm_mday = cur.tm_mday - 1;
-    }
-
-    // Months
-    if (mon + 1 == 13) {
-        next->tm_mon = 0;
-    } else {
-        next->tm_mon = cur.tm_mon + 1;
-    }
-    if (mon - 1 == 0) {
-        last->tm_mon = 11;
-    } else {
-        last->tm_mon = cur.tm_mon - 1;
-    }
-
-    // Years
-    next->tm_year = cur.tm_year + 1;
-    last->tm_year = cur.tm_year - 1;
-
-    get_last_next_hours_mins_or_seconds(cur.tm_hour, 24, &last->tm_hour,
-                                        &next->tm_hour); // Hours
-    get_last_next_hours_mins_or_seconds(cur.tm_min, 60, &last->tm_min,
-                                        &next->tm_min); // Minutes
-    get_last_next_hours_mins_or_seconds(cur.tm_sec, 60, &last->tm_sec,
-                                        &next->tm_sec); // Seconds
-}
-
-// Return the days that a month has
-int days_in_month(int mon, int year)
-{
-    assert(mon >= 1 && mon <= 12);
-    switch (mon) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        return 31;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        return 30;
-    case 2:
-        // Leap year: https://en.wikipedia.org/wiki/Leap_year#Algorithm
-        if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))
-            return 29;
-        else
-            return 28;
-    default:
-        assert(false);
-        return 0;
-    }
 }
